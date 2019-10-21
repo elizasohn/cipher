e2747cdfb751f54b448930124362d3d554022c3e
Student <example@example.com>
2019-10-21 16:53:58 -0700

    make it a function

--- scripts.js
+++ scripts.js
@@ -1,19 +1,24 @@
 var input = prompt("enter a sentence");
-console.log(input)
-alert("you said: " + input);
+
+var reverse = function(input) {
+  console.log(input)
+  alert("you said: " + input);
 
 
-var first = input.charAt(0);
-var last = input.charAt(input.length-1);
-alert("The first letter of the sentence was " + first + " and the last letter was " + last);
+  var first = input.charAt(0);
+  var last = input.charAt(input.length-1);
+  alert("The first letter of the sentence was " + first + " and the last letter was " + last);
 
-var firstUpperCase = first.toUpperCase();
-var lastUpperCase = last.toUpperCase();
+  var firstUpperCase = first.toUpperCase();
+  var lastUpperCase = last.toUpperCase();
 
-var inputnofirstandlast = input.substring(1, input.length -1);
-var capfirstandlast = firstUpperCase + inputnofirstandlast + lastUpperCase;
+  var inputnofirstandlast = input.substring(1, input.length -1);
+  var capfirstandlast = firstUpperCase + inputnofirstandlast + lastUpperCase;
 
-alert(capfirstandlast);
+  alert(capfirstandlast);
 
-var reverse = last + inputnofirstandlast + first;
-alert(reverse);
+  return last + inputnofirstandlast + first;
+}
+
+
+alert(reverse(input));
